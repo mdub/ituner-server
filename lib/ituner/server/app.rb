052a58e9d9f29a2fe29f24f5e5999aff38242f36
Mike Williams <mdub@dogbiscuit.org>
2011-03-19 20:50:39 +1100

    Move to modular Sinatra app.

--- lib/ituner/server/app.rb
+++ lib/ituner/server/app.rb
@@ -1,42 +1,53 @@
 require 'haml'
 require 'ituner'
 require 'json'
-require 'sinatra'
-require 'sinatra/reloader' if development?
-
+require 'sinatra/base'
+require 'sinatra/reloader'
 require 'ituner/server/database'
 
-set :app_file, __FILE__
-set :haml, :format => :html5
+module ITuner
+  module Server
+    class App < Sinatra::Base
 
-helpers do
+      set :app_file, __FILE__
+      set :haml, :format => :html5
 
-  def search
-    search_term = params["term"]
-    if search_term
-      @search_results = ITuner.itunes.music.search(search_term)
+      configure(:development) do
+        register Sinatra::Reloader
+      end
+
+      helpers do
+
+        def search
+          search_term = params["term"]
+          if search_term
+            @search_results = ITuner.itunes.music.search(search_term)
+          end
+        end
+
+        def playing?
+          ITuner.itunes.playing?
+        end
+
+      end
+
+      get '/style.css' do
+        scss :style
+      end
+
+      get '/' do
+        @current_track = ITuner.itunes.current_track
+        search
+        haml :home
+      end
+
+      # get '/search.json' do
+      #   search
+      #   @track_data = @tracks.map(&:name)
+      #   content_type :json
+      #   @track_data.to_json
+      # end
+
     end
   end
-
-  def playing?
-    ITuner.itunes.playing?
-  end
-
 end
-
-get '/style.css' do
-  scss :style
-end
-
-get '/' do
-  @current_track = ITuner.itunes.current_track
-  search
-  haml :home
-end
-
-# get '/search.json' do
-#   search
-#   @track_data = @tracks.map(&:name)
-#   content_type :json
-#   @track_data.to_json
-# end
